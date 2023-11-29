52f00e241408e82c5c671e22b756e5aa66f074be
robimuller <mullerrobi93@gmail.com>
2023-11-29 10:36:11 +0100

    Initial Commit

---
+++ crm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  crm
+//
+//  Created by Robert Muller on 29/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ crm/crmApp.swift
@@ -0,0 +1,17 @@
+//
+//  crmApp.swift
+//  crm
+//
+//  Created by Robert Muller on 29/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct crmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
